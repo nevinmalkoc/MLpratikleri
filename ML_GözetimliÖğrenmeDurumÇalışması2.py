be48ba05d1af15dc2786c5e7bdc50778991fe768
Nevin Malkoç <nevinmalkoc@hotmail.com>
2025-10-08 17:59:05 +0300

    Add files via upload

---
+++ ML_GözetimliÖğrenmeDurumÇalışması2.py
@@ -0,0 +1,24 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+housing= fetch_california_housing()
+
+X =housing.data
+y= housing.target
+
+X_train, X_test ,y_train ,y_test= train_test_split(X, y, test_size = 0.2 ,  random_state =42)
+
+poly_feat = PolynomialFeatures(degree= 2)
+X_train_poly = poly_feat.fit_transform(X_train)
+X_test_poly = poly_feat.transform(X_test)
+
+poly_reg = LinearRegression()
+poly_reg.fit(X_train_poly , y_train)
+y_pred = poly_reg.predict(X_test_poly)
+
+#print("polynomial Regression rsme: ", mean_squared_error(y_test, y_pred, squared =False))
+import numpy as np
+print("Polynomial Regression RMSE:", np.sqrt(mean_squared_error(y_test, y_pred)))
